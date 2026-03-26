64e467c3fa34f60967cbdc725378e1199f60c7a8
Steeven Toala <steevenpfb@gmail.com>
2026-03-26 18:25:38 -0500

    script de flask con postgress con tabla estudiantes

--- practica_clase/practica_flask.py
+++ practica_clase/practica_flask.py
@@ -1,47 +1,43 @@
 from flask import Flask, jsonify
-import pyodbc
+import psycopg2
 import pdb
-
 app = Flask(__name__)
 
 def get_connection():
-    return pyodbc.connect(
-        "DRIVER={OBDC Driver 17 for SQL Server};"
-        "SERVER=10.79.0.78;" #ip del SQLServer
-        "DATABASE=AppDistribuidasDB_P;"
-        "UID=sa;"
-        "PWD=sa;"
-        "TrustServerCertificate=yes;"
-    )
-
-@app.route('/api/productos', methods=['GET'])
-def get_productos():
+    """Conexión a la base de datos PostgreSQL"""
     try:
-        conn = get_connection()
-        cursor = conn.cursor()
-        cursor.execute("SELECT id, nombre, precio FROM productos")
-        rows = cursor.fetchall()
-        productos = []
-        for row in rows:
-            productos.append({
-                'id': row.Id,
-                'nombre': row.Nombre,
-                'precio': row.Precio
-            })
-        
-        cursor.close()
-        conn.close()
-        return jsonify(productos) , 200
+        connection = psycopg2.connect(
+            host="localhost",
+            database="prueba",
+            user="postgres",
+            password="12345678",
+            port="5439"
+        )
+        return connection
     except Exception as e:
-        return jsonify(
-                {
-                'error': "error al obtener productos",
-                'details': str(e)
-                }
-            ), 500
-    finally:
+        print("Error al conectar:", e)
+        return None
+    
+
+@app.route("/api/estudiantes", methods=["GET"])
+def get_estudiantes():
+    """Endpoint para obtener la lista de estudiantes"""
+    conn = get_connection()
+    pdb.set_trace()  # Punto de interrupción para depuración
+    if conn:
+        cursor = conn.cursor()
+        cursor.execute("SELECT id, cedula,nombres, apellidos, direccion, fecha_nacimiento FROM estudiantes")
+        estudiantes = cursor.fetchall()
         cursor.close()
         conn.close()
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=500, debug=False)
+        
+        # Convertir a formato JSON
+        estudiantes_list = [{"id": e[0], "cedula": e[1], "nombres": e[2], "apellidos": e[3], "direccion": e[4], "fecha_nacimiento": e[5]} for e in estudiantes]
+        return jsonify(estudiantes_list)
+    else:
+        return jsonify({"error": "No se pudo conectar a la base de datos"}), 500
+    
+    
+    
+if __name__ == "__main__":
+    app.run(host="0.0.0", port=5070,debug=False)
